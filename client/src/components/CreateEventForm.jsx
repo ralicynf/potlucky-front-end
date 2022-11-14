8c9785e56d3296add6bc4a331ad03767f49b3a70
rebecca <fulkerson.rebecca@gmail.com>
2022-11-14 03:29:11 -0500

    restructured the createventform

--- client/src/components/CreateEventForm.jsx
+++ client/src/components/CreateEventForm.jsx
@@ -32,35 +32,45 @@ const CreateEventForm = () => {
     return (
         <div className="the-form">
             <div onSubmit={handleSubmit}>
-                <label htmlFor="Event Name">Event Name:</label>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        id="eventName"
-                        value={formState.eventName}
-                    />
-                <label htmlFor="Event Date">Event Date:</label>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        id="eventDate"
-                        value={formState.eventDate}
-                    />
-                <label htmlFor="Event Location">Event Location:</label>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        id="eventLocation"
-                        value={formState.eventLocation}
-                    />
-                <label htmlFor="Event Description">Event Description:</label>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        id="eventDescription"
-                        value={formState.eventDescription}
-                    />
-                <button type="submit">Submit</button>
+                <div>
+                    <label htmlFor="Event Name">Event Name:</label>
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            id="eventName"
+                            value={formState.eventName}
+                        />
+                </div>
+                <div>
+                    <label htmlFor="Event Date">Event Date:</label>
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            id="eventDate"
+                            value={formState.eventDate}
+                        />
+                </div>
+                <div>
+                    <label htmlFor="Event Location">Event Location:</label>
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            id="eventLocation"
+                            value={formState.eventLocation}
+                        />
+                </div>
+                <div>
+                    <label htmlFor="Event Description">Event Description:</label>
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            id="eventDescription"
+                            value={formState.eventDescription}
+                        />
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
             </div>
         </div>
     )
