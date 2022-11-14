f61fa008d1ed80aad8ea30718a449b4e099dd740
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-14 15:45:30 -0600

    changes to nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,42 @@
 import { Link } from 'react-router-dom'
 
-const NavBar = () => {
-
-    return (
-        <header>
-            <nav>
-                <div id="nav-logo">
-                    <h1>Potlucky</h1>
-                </div>
-                <div></div>
-                <div className="nav-links">
-                    <Link to='/'>Home</Link>
-                    <Link to='/register'>Register</Link>
-                    <Link to='/signin'>SignIn</Link>
-                    <Link to='/profile'>Profile</Link>
-                    <Link to='/newevent'>Create Event</Link>
-                </div>
-            </nav>
-        </header>
-
+const NavBar = ({ user, handleLogout }) => {
+  let authenticatedOptions
+  if (user) {
+    authenticatedOptions = (
+      <nav>
+        <h3>Welcome {user.email}!</h3>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/newevent">Create Event</Link>
+        <Link onClick={handleLogout} to="/">
+          Sign Out
+        </Link>
+      </nav>
     )
+  }
+
+  const publicOptions = (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/signin">Sign In</Link>
+    </nav>
+  )
+
+  return (
+    <header>
+      <nav>
+        <div id="nav-logo">
+          <h1>Potlucky</h1>
+        </div>
+        <div></div>
+        <div className="nav-links">
+          {user ? authenticatedOptions : publicOptions}
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default NavBar
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,29 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const Profile = (user) => {
-   let navigate = useNavigate()
-    
-    return (
-        <div>
-            <div> 
-                <h1>Welcome!</h1>
-            </div>
-            <div>
-                <h3>User Details</h3>
-                <h6>Name: </h6>
-                <h6>Username:</h6>
-            </div>
-            <div>
-                <h3>Events Dashboard</h3>
-            </div>
-        </div>
-    )
+const Profile = ({ user }) => {
+  let navigate = useNavigate()
 
+  return user ? (
+    <div>
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+      <div>
+        <h3>User Details</h3>
+        <h6>Name: </h6>
+        <h6>Username:</h6>
+      </div>
+      <div>
+        <h3>Events Dashboard</h3>
+      </div>
+    </div>
+  ) : (
+    <div className="protected">
+      <h3>Oops! You must be signed in to do that!</h3>
+      <button onClick={() => navigate('/signin')}>Sign In</button>
+    </div>
+  )
 }
 
 export default Profile
