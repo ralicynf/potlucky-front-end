692cd36bb45dd50f3e440a2f6fa881bc1af615c9
rebecca <fulkerson.rebecca@gmail.com>
2022-11-14 03:04:58 -0500

    added services files for Auth

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Register = () => {
    let navigate = useNavigate()
+
+   const startState = {
+        username: '',
+        name: '',
+        email: '',
+        password: ''
+   }
+
+   const [formState, setFormState] = useState(startState)
+
+   const handleChange = (e) => {
+    setFormState({...formState, [e.target.name]: e.target.value})
+   }
+
+   const handleSubmit = async (e) => {
+   }
     
     return (
         <div>
@@ -13,3 +30,5 @@ const Register = () => {
 }
 
 export default Register
+
+//username, name, email, password, 
---
+++ client/src/services/api.js
@@ -0,0 +1,4 @@
+import Axios from 'axios'
+
+export const BASE_URL = 'http://localhost:3001'
+
