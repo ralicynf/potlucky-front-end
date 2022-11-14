f65a01de848c47492753119946c1b21a2cbca1c1
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-14 13:25:07 -0600

    added services and implemented auth for Register and SignIn pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,51 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import NewEvent from './pages/NewEvent';
-import Profile from './pages/Profile';
-import Register from './pages/Register';
-import SignIn from './pages/Signin';
+import './App.css'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import NewEvent from './pages/NewEvent'
+import Profile from './pages/Profile'
+import Register from './pages/Register'
+import SignIn from './pages/Signin'
 import { Routes, Route } from 'react-router-dom'
-import EventDetails from './pages/EventDetails';
+import EventDetails from './pages/EventDetails'
+import { CheckSession } from './services/Auth'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
   return (
     <div className="App">
       <NavBar />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element= { <Register /> } />
-          <Route path='/signin' element= { <SignIn /> } />
-          <Route path='/newevent' element= { <NewEvent /> } />
-          <Route path='/profile' element= { <Profile /> } />
-          <Route path='/event' element= { <EventDetails /> } /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/newevent" element={<NewEvent />} />
+          <Route path="/profile" element={<Profile user={user} />} />
+          <Route path="/event" element={<EventDetails />} />
         </Routes>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,55 +1,59 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import { SignInUser } from '../services/Auth'
 
-const SignIn = () => {
-   let navigate = useNavigate()
+const SignIn = ({ setUser }) => {
+  let navigate = useNavigate()
 
-   const startState = {
-        username: '',
-        password: ''
-   }
+  const startState = {
+    email: '',
+    password: ''
+  }
 
-   const [formState, setFormState] = useState(startState)
+  const [formState, setFormState] = useState(startState)
 
-   const handleChange = (e) => {
-    setFormState({...formState, [e.target.name]: e.target.value})
-   }
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
 
-   const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formState)
+    setUser(payload)
+    setFormState(startState)
+    navigate('/profile')
+  }
 
-   }
-    
-    return (
-        <div className='signin'>  
-            <div className='sign-in-box' onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='username'>Username</label>
-                    <input 
-                        onChange={handleChange}
-                        name='username'
-                        type='text'
-                        placeholder='username'
-                        value={formState.username}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor='password'>Password</label>
-                    <input 
-                        onChange={handleChange}
-                        name='password'
-                        type='text'
-                        placeholder='password'
-                        value={formState.password}
-                        required
-                    />
-                </div>
-                <button>Sign in!</button>
-            </div> 
+  return (
+    <div className="signin">
+      <form className="sign-in-box" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            name="email"
+            type="text"
+            placeholder="email"
+            value={formState.email}
+            required
+          />
         </div>
-    )
-
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            placeholder="password"
+            value={formState.password}
+            required
+          />
+        </div>
+        <button>Sign in!</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignIn
