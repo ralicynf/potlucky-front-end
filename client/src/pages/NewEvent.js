62d6040cae861d9f8bb7fb47206611bc6f6b7cb0
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-15 09:28:39 -0600

    create event submit

--- client/src/pages/NewEvent.js
+++ client/src/pages/NewEvent.js
@@ -1,15 +1,12 @@
 import CreateEventForm from '../components/CreateEventForm'
 
-const NewEvent = () => {
-   
-    
-    return (
-        <div className='event-form'>
-            <h1>Create New Event</h1>
-            <CreateEventForm />
-        </div>
-    )
-
+const NewEvent = ({ user }) => {
+  return (
+    <div className="event-form">
+      <h1>Create New Event</h1>
+      <CreateEventForm user={user} />
+    </div>
+  )
 }
 
 export default NewEvent
