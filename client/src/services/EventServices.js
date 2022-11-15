4e3c0d943c953b1f8f6cea9ce16c20af1be6fd65
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-15 10:23:51 -0600

    fixed form in create event from component

--- client/src/services/EventServices.js
+++ client/src/services/EventServices.js
@@ -2,7 +2,6 @@ import Client from './api'
 
 export const CreateEvent = async (formData) => {
   try {
-    console.log(formData)
     await Client.post('/events', formData)
   } catch (error) {
     throw error
