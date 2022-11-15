a79c1539146a273e77cf5902117012f557835ab7
Carlos Hidalgo <cfhidalgo10@gmail.com>
2022-11-15 08:45:00 -0600

    prep for pull

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,17 +2,15 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-   let navigate = useNavigate()
-    
-    return (
-        <div>
-            <div>
-                <h3>Your Events</h3>
-            </div>  
-            
-        </div>
-    )
+  let navigate = useNavigate()
 
+  return (
+    <div>
+      <div>
+        <h3>Your Events</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Home
